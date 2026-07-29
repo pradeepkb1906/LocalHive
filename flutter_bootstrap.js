2fdfaf11945afc271d367470d9a894838844d739
Pradeep Basavarajappa <pradeep.basavarajappa2@ibm.com>
2026-07-29 22:43:15 +0530

    Deploy LocalHive PWA (guided demo tour)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2894271053" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1018782103" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
