ea1f00ac6900b3fe7a53c9b6d7134da8feecd326
Pradeep Basavarajappa <pradeep.kariganur@gmail.com>
2026-07-30 20:01:53 +0530

    Deploy: Nearby Now live map + sign-in front door (--no-verify: same two known bundle false positives)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3277555921" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3965509772" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
