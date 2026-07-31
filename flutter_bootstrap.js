3563ac2ad9920d1b500cd7a6e1d3698833dd51ff
Pradeep Basavarajappa <pradeep.basavarajappa2@ibm.com>
2026-07-31 11:44:19 +0530

    Deploy: booking timestamps, In progress/Past split, real dates, double-submit guard

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4152476771" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2159793167" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
